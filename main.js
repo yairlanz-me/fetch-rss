c8bfb0a095684698c04a3b17e2bd4570ae9de1e2
yairlanz-me <yairlanz@outlook.com>
2024-11-04 21:21:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-//const cl = (txt)=> console.log(txt);
+const cl = (txt)=> console.log(txt);
 import { parse } from "https://deno.land/x/xml@6.0.0/mod.ts";
 const url = Deno.env.get("PANTRY_URL")
-
-async function Get(url) {
-  const res = await fetch(url); const data = await res.json(); return data;
-}
-async function Post(url,payload) {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  const res = await fetch(url, {method: "POST", body: JSON.stringify(payload), headers: myHeaders});
-  return res;
-}
 
 FetchRss(); 
 Deno.cron("Cron rss",  "*/10 * * * *", FetchRss)
@@ -35,3 +25,12 @@ async function FetchRss(){
   Post(url, rssObj)
   cl('Refreshed...' + rssObj.lastUpdt.toLocaleString());
 } 
+async function Get(url) {
+  const res = await fetch(url); const data = await res.json(); return data;
+}
+async function Post(url,payload) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  const res = await fetch(url, {method: "POST", body: JSON.stringify(payload), headers: myHeaders});
+  return res;
+}
